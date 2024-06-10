191b5402215e9ed919f03ab83034e1e42acaee75
Zakhar Shokel <zakhar.shokel@paysera.net>
2024-06-10 12:24:41 +0300

    add legacy Paysera\Bundle\ApiBundle\Service\Annotation\ReflectionMethodWrapper to resolve the BC

---
+++ src/Service/Annotation/ReflectionMethodWrapper.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Paysera\Bundle\ApiBundle\Service\Annotation;
+
+use Paysera\Bundle\ApiBundle\Service\RoutingLoader\ReflectionMethodWrapper as RoutingLoaderReflectionMethodWrapper;
+
+/**
+ * @internal
+ * @deprecated Use {@link RoutingLoaderReflectionMethodWrapper} instead
+ */
+class ReflectionMethodWrapper extends RoutingLoaderReflectionMethodWrapper
+{
+}
